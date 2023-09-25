f2200e36c207f5d58884c814883b7241e7b66f30
Parag <paraggaigawali123@gmail.com>
2023-09-25 21:18:03 +0530

    First setup commit

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+HYPEN_E_dot = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function will return the list of requirements
+    '''
+
+    requirements = []
+
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()    
+        requirements = [req.replace('\n',' ') for req in requirements]
+
+        if HYPEN_E_dot in requirements:
+            requirements.remove(HYPEN_E_dot)
+
+    return requirements
+
+
+
+
+setup(
+name='MLProject',
+version='0.0.1',
+author='Parag',
+author_email='paraggaigawali123@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+)
