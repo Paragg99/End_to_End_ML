22cb20200dac354a8dd1b4c67abd507a77b18f73
Parag <paraggaigawali123@gmail.com>
2023-09-27 21:56:04 +0530

    Data Transformation

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from src.MLProject.logger import logging
 from src.MLProject.exception import CustomException
 from src.MLProject.components.data_ingestion import DataIngestion
 from src.MLProject.components.data_ingestion import DataIngestionConfig
+from src.MLProject.components.data_transformation import DataTransformationConfig,DataTransformation
 
 
 
@@ -15,9 +16,13 @@ if __name__ == "__main__":
     try:
         #data_ingestion_config = DataIngestionConfig()
         data_ingestion = DataIngestion()
-        data_ingestion.initiate_data_ingestion()
+        train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
 
-        
+        #data_tranformation_config = DataTransformationConfig()
+        data_tranformation = DataTransformation()
+        data_tranformation.initiate_data_transformation(train_data_path,test_data_path)
+
+
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e, sys)
--- src/MLProject/utils.py
+++ src/MLProject/utils.py
@@ -4,7 +4,9 @@ from src.MLProject.exception import CustomException
 from src.MLProject.logger import logging
 import pandas as pd
 import pymysql
-import dotenv 
+import dotenv
+import pickle
+import numpy as np
 
 dotenv.load_dotenv()
 
@@ -32,5 +34,18 @@ def read_sql_data():
 
 
 
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+
+def save_object(file_path,obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, 'wb') as file_obj:
+            pickle.dump(obj,file_obj)
+    
     except Exception as e:
         raise CustomException(e,sys)
